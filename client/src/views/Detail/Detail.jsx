8cd1a984f56f4ce6b3143e14e40aa56228023a5c
juanfacundotam <facutam@gmail.com>
2023-04-18 21:03:10 -0300

    Front 8

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -17,54 +17,76 @@ const Detail = () => {
       dispatch(cleanDetail());
     };
   }, []);
-  
 
   const getInstructions = () => {
-    console.log(typeof instructions === "object")
-    if(typeof instructions === "object"){
+    console.log(typeof instructions === "object");
+    if (typeof instructions === "object") {
       const arrayInstructions = Object.entries(instructions);
-      const results = arrayInstructions.map(elem => {
-
-        return (<p className={style.instruction}>{`${elem[0]} -> ${elem[1]}`}</p>)
-      })
-
+      const results = arrayInstructions.map((elem) => {
+        return (
+          <div className={style.divInstructionsItem}>
+            <p className={style.instructionNumber}>{elem[0]}</p>
+            <p className={style.instructionText}>{elem[1]}</p>
+          </div>
+        );
+      });
       return results;
     }
-
-    return <div dangerouslySetInnerHTML={{ __html: instructions }} />
-  }
-
-
-  if (
-    !id ||
-    !title ||
-    !image
-    // !healthscore ||
-    // !summary ||
-    // !instructions ||
-    // !diets
-  ) {
-    return <div className={style.customLoader}></div>;
-  }
-
-  return (
-    <div className={style.container}>
-
-          <div className={style.divDetail}>
+    return (
+      <div
+      className={style.divInstructions}
+      dangerouslySetInnerHTML={{ __html: instructions }}
+      />
+      );
+  };
+  
+  // if (
+  //   !id ||
+  //   !title ||
+  //   !image
+  //   // !healthscore ||
+  //   // !summary ||
+  //   // !instructions ||
+  //   // !diets
+  //   ) {
+  //     return <div className={style.customLoader}></div>;
+  //   }
+    
+    return (
+      <div className={style.container}>
+        {image ? (<>
+          <div className={style.detailWall}></div>
+      <div className={style.divDetail}>
+        <div className={style.divTop}>
+          <img src={image} alt="" className={style.image} />
+          <div className={style.divTopRight}>
             <Link to={"/home"}>
               <button className={style.button}>&lt;&lt;</button>
             </Link>
             <h1 className={style.title}>{title}</h1>
 
             <p className={style.healthscore}>health score: {healthscore}</p>
-            <div dangerouslySetInnerHTML={{ __html: summary }} />
-
-            {getInstructions()}
+            <div className={style.divDiets}>
             {diets?.map((diet) => {
-              return <p className={style.diet}>{diet}</p>;
+              return <p className={style.diet}>&lt;{diet}&gt;</p>;
             })}
-            <img src={image} alt="" className={style.image} />
+            </div>
           </div>
+        </div>
+            <div className={style.divSummary}>
+            <h3 className={style.summaryTitle}>Summary</h3>
+            <div dangerouslySetInnerHTML={{ __html: summary }} className={style.summary}/>
+
+            </div>
+        <div className={style.divInstructions}>
+            <h3>Instructions</h3>
+        {getInstructions()}
+        </div>
+      </div>
+      </>
+      ) : (
+        <div className={style.customLoader}></div>
+      )}
     </div>
   );
 };
