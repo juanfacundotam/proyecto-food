6e3e945bc2e9607682b071bb283648d5212817e7
juanfacundotam <facutam@gmail.com>
2023-04-26 12:26:02 -0300

    actualizando

--- api/src/controllers/getRecipesQuery.js
+++ api/src/controllers/getRecipesQuery.js
@@ -1,27 +1,45 @@
-require('dotenv').config();
-const {
-  URL_API, API_KEY
-} = process.env;
+require("dotenv").config();
+const { URL_API, API_KEY } = process.env;
 const axios = require("axios");
 const { Recipe, Diet } = require("../db");
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 
 const getRecipeByQuery = async (title) => {
+  console.log("entro");
   const recipeBdd = await Recipe.findAll({
     where: {
       title: {
         [Op.iLike]: `%${title}%`,
       },
     },
+    include: {
+      model: Diet,
+      attributes: ["id", "name"],
+      through: {
+        attibutes: [],
+      },
+    },
   });
 
-  let recipeApi = await axios.get(`${URL_API}/recipes/complexSearch?${API_KEY}&query=${title}`);
-  recipeApi = recipeApi.data.results;
-  
-  const result = [...recipeBdd, ...recipeApi];
-  if(result.length === 0) throw Error ("Recipe not found");
+  const recipeBddMaped = recipeBdd.map((recipe) => recipe.dataValues);
+  const recipeBddWithDiets = recipeBddMaped.map((recipe) => {
+    const result = {
+      ...recipe,
+      diets: recipe.Diets.flatMap((diet) => diet.name),
+    };
+    return result;
+  });
 
-  return result;
+  let recipeApi = await axios.get(
+    `${URL_API}/recipes/complexSearch?${API_KEY}&addRecipeInformation=true&query=${title}`
+  );
+
+  recipeApi = recipeApi.data.results;
+
+  const results = [...recipeBddWithDiets, ...recipeApi];
+  if (results.length === 0) throw Error("Recipe not found");
+
+  return results;
 };
 
 module.exports = getRecipeByQuery;
